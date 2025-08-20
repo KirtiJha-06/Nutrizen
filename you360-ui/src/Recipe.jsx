626b92f1df768bd04cbd7a1e87ff4688e0511bc0
Kirti Jha <163504689+KirtiJha-06@users.noreply.github.com>
2025-08-20 23:27:37 +0530

    Update Recipe.jsx

--- you360-ui/src/Recipe.jsx
+++ you360-ui/src/Recipe.jsx
@@ -50,7 +50,7 @@ const App = () => {
           }
         }
       };
-      const apiKey = "process.env.VITE_GEMINI_API_KEY";
+          const apiKey = "process.env.VITE_GEMINI_API_KEY"; 
       const apiUrl = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-preview-05-20:generateContent?key=${apiKey}`;
 
       const response = await fetch(apiUrl, {
